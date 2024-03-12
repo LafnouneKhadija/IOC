46e2d09946bbb31fbe9f4f2e8cb8fce390caae51
unknown <lafnoune.khadija23@gmail.com>
2024-03-12 08:28:19 +0000

    thirdv4 commit

--- src/main/java/Pres/pres2.java
+++ src/main/java/Pres/pres2.java
@@ -1,16 +1,12 @@
 package Pres;
 
-
 import Dao.Idao;
 import Metier.Imetier;
-
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.Scanner;
-
 public class pres2  {
     public static void main(String[] args) throws Exception{
-
 
             Scanner scanner=new Scanner(new File("config.txt"));
             String daoClassName=scanner.nextLine();
